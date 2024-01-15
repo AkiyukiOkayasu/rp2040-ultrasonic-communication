1168c1ecb8ac1b0a6251756cd86d0e5b037ab94f
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2024-01-15 18:54:55 +0900

    on-chip voltage regulatorの設定関数のコメント追加とかメモリレイアウトの明示とか

--- src/vreg.rs
+++ src/vreg.rs
@@ -13,32 +13,33 @@ use crate::pac::VREG_AND_CHIP_RESET;
 
 /// Possible voltage values that can be applied to the regulator
 #[allow(dead_code)]
+#[repr(u8)]
 pub enum VregVoltage {
     /// 0.85V
-    Voltage0_85 = 0b0110,
+    Voltage0_85 = 0b0110u8,
     /// 0.90V   
-    Voltage0_90 = 0b0111,
+    Voltage0_90 = 0b0111u8,
     /// 0.95V
-    Voltage0_95 = 0b1000,
+    Voltage0_95 = 0b1000u8,
     /// 1.00V
-    Voltage1_00 = 0b1001,
+    Voltage1_00 = 0b1001u8,
     /// 1.05V
-    Voltage1_05 = 0b1010,
+    Voltage1_05 = 0b1010u8,
     /// 1.10V (default)
-    Voltage1_10 = 0b1011,
+    Voltage1_10 = 0b1011u8,
     /// 1.15V
-    Voltage1_15 = 0b1100,
+    Voltage1_15 = 0b1100u8,
     /// 1.20V
-    Voltage1_20 = 0b1101,
+    Voltage1_20 = 0b1101u8,
     /// 1.25V
-    Voltage1_25 = 0b1110,
+    Voltage1_25 = 0b1110u8,
     /// 1.30V
-    Voltage1_30 = 0b1111,
+    Voltage1_30 = 0b1111u8,
 }
 
 /// Set voltage
 ///
-/// voltage  The voltage (from enumeration \ref vreg_voltage) to apply to the voltage regulator
+/// * `voltage` - The voltage (from enumeration \ref vreg_voltage) to apply to the voltage regulator
 pub fn vreg_set_voltage(vreg_dev: &mut VREG_AND_CHIP_RESET, voltage: VregVoltage) {
     vreg_dev
         .vreg
